9feda38d03f6411eae4aeac8aea482cc982a8b75
RallapalliGovardhanReddy2003 <govardhan.rallpalli@gmail.com>
2025-12-12 11:54:11 +0530

    curd project intial

--- src/test/java/demosprings/service/UserServiceImplTest.java
+++ src/test/java/demosprings/service/UserServiceImplTest.java
@@ -5,7 +5,7 @@ import demosprings.exception.ResourceNotFoundException;
 import demosprings.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
 
 import java.util.Optional;
 
